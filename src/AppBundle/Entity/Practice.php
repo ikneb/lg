738ec0c46b14eb60ef1b384051ac979ec0fe8073
ikneb <ikneblis@gmail.com>
2016-10-22 20:03:23 +0300

    Created entity and  table practice

---
+++ src/AppBundle/Entity/Practice.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ikneb
+ * Date: 22.10.2016
+ * Time: 19:45
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * @ORM\Table(name="practic")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SkillRepository")
+ **/
+class Practic
+{
+
+}
---
+++ src/AppBundle/Repository/PracticeRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ikneb
+ * Date: 22.10.2016
+ * Time: 19:47
+ */
+
+namespace AppBundle\Repository;
+
+
+class PracticeRepository
+{
+
+}
